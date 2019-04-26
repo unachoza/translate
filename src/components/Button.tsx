6e4f021da97009a82ae2984274f7205ba021fc6a
Arianna Choza <ariannachoza@Ariannas-MacBook-Pro.local>
2019-04-26 15:58:50 -0400

    context: source data with default value

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,11 @@
 import React, {Component} from 'react'
+import LanguageContext from '../contexts/LanguageContexts'
 
  class Button extends Component{
+    static contextType = LanguageContext
+
      render(){
+         console.log(this.context)
          return (
              <div className=" ui button primary">
                 Submit
